706da04b74dca29ba611ebbae14371d9a592b40b
micaelhjs <micaelhjs>
2023-10-18 22:03:25 -0300

    Menu Navegacao 1.2

--- paginas/urls.py
+++ paginas/urls.py
@@ -1,7 +1,16 @@
 from django.urls import path
-from .views import IndexView
+from .views import IndexView, BlogView, ColetaView,EmpresaColetaView, AgendarColetaView, HistoricoColetaView, DescarteView, LocalDescarteView, AgendarDescarteView, HistoricoDescarteView
 
-urlpatterns = {
+urlpatterns = [
     #Modelo de criação de url: path('endereco/',NomedaView.as.view(),name='nome_da_url'),
-    path('',IndexView.as_view(), name='inicio'), #Pagina Inicial
-}
+    path('',IndexView.as_view(), name='index'), #Pagina Inicial
+    path('blog/', BlogView.as_view(), name='blog'), #Blog
+    path('coleta/', ColetaView.as_view(), name='coleta'),
+    path('coleta/empresacoleta', EmpresaColetaView.as_view(), name='empresacoleta'),
+    path('coleta/agendarcoleta', AgendarColetaView.as_view(), name='agendarcoleta'),
+    path('coleta/historicocoleta',HistoricoColetaView.as_view(), name='historicocoleta'),
+    path('descarte/', DescarteView.as_view(), name='descarte'),
+    path('descarte/localdescarte',LocalDescarteView.as_view(), name='localdescarte'),
+    path('descarte/agendardescarte', AgendarDescarteView.as_view(), name='agendardescarte'),
+    path('descarte/historidescarte',HistoricoDescarteView.as_view(), name='historicodescarte'),
+]
--- paginas/views.py
+++ paginas/views.py
@@ -1,4 +1,32 @@
 from django.views.generic import TemplateView
 
 class IndexView(TemplateView):
-    template_name = "modelo.html"
+    template_name = "index.html"
+
+
+class BlogView(TemplateView):
+    template_name = "blog.html"
+
+class ColetaView(TemplateView):
+    template_name = "coleta.html"
+
+class EmpresaColetaView(TemplateView):
+    template_name = "empresacoleta.html"
+
+class AgendarColetaView(TemplateView):
+    template_name = "agendarcoleta.html"
+
+class HistoricoColetaView(TemplateView):
+    template_name = "historicocoleta.html"
+
+class DescarteView(TemplateView):
+    template_name = "descarte.html"
+
+class LocalDescarteView(TemplateView):
+    template_name = "localdescarte.html"
+
+class AgendarDescarteView(TemplateView):
+    template_name = "agendardescarte.html"
+
+class HistoricoDescarteView(TemplateView):
+    template_name = "historicodescarte.html"
